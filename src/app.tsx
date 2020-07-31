155823cf008e46ae2d0594ff378fe196136a7e3b
Ivan.Yakovlev <ivan.yakovlev@pochtavip.com>
2020-07-31 15:04:59 +0300

    trash remove

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,6 @@
 import { Component, Vue } from 'vue-property-decorator';
 import InputArea from './components/InputArea';
 import TextArea from './components/TextArea';
-import {IState} from '@/store/state';
-import { State, Action, Getter } from 'vuex-class';
 
 import styles from './app.css?module';
 
